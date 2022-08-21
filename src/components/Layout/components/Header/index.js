9fbc067d5e4e5f0e36e2691cc59795b54683791a
tuannt02@ubuntu <20522122@gm.uit.edu.vn>
2022-08-21 09:33:07 +0700

    Frame DefaultLayout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+// bind cai' object style nay vao roi tra ra function. Roi dung cai nay lam class
+// thi khi do giup cac ban viet duoi dang dau gach ngang
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
