66da2c0003de1aeaca2812aa1962cf80df6fda8b
tuannt02 <20522122@gm.uit.edu.vn>
2022-08-21 23:48:44 +0700

    Header (logo, search)

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,9 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 // bind cai' object style nay vao roi tra ra function. Roi dung cai nay lam class
 // thi khi do giup cac ban viet duoi dang dau gach ngang
@@ -8,7 +12,21 @@ const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input className={cx('input')} placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        {/* <img src={images.search} alt="Search icon" /> */}
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>Actions</div>
+            </div>
         </header>
     );
 }
