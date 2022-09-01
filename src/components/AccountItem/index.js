f50c8a6f2174f82f7c4b3e78419e904c2e34f6db
tuannt02 <20522122@gm.uit.edu.vn>
2022-09-01 16:21:03 +0700

    logic for search functionality (call API)

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,5 +1,6 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 import Image from '~/components/Image';
 import classNames from 'classnames/bind';
@@ -7,22 +8,18 @@ import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
-        <div className={cx('wrapper')}>
-            <Image
-                className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/557695794d1cb0b4a32322da33aac45d~c5_300x300.webp?x-expires=1661385600&x-signature=bgGfVdUdeXWfn0ELF4Ex8dEFDR0%3D"
-                alt="lebong95"
-            />
+        <Link to={`@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.nickname} />
             <div className={cx('info')}>
                 <h4 className={cx('username')}>
-                    <span>lebong95</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <span>{data.nickname}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
-                <p className={cx('name')}>Lê Thị Khánh Huyền</p>
+                <p className={cx('name')}>{data.full_name}</p>
             </div>
-        </div>
+        </Link>
     );
 }
 
--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -1,13 +1,13 @@
-import { useEffect, useState, useRef } from 'react';
 import classNames from 'classnames/bind';
+import { useEffect, useRef, useState } from 'react';
 
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import TippyHeadless from '@tippyjs/react/headless';
 
-import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import Icon from '~/components/Icon';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles);
@@ -16,14 +16,28 @@ function Search() {
     const [searchResult, setSearchResult] = useState([]);
     const [searchText, setSearchText] = useState('');
     const [isShowResult, setIsShowResult] = useState(true);
+    const [isProcessingAPI, setIsProcessingAPI] = useState(false);
 
     const input = useRef();
 
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1]);
-        }, 0);
-    }, []);
+        if (!searchText.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        setIsProcessingAPI(true);
+
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchText)}&type=less`)
+            .then((res) => res.json())
+            .then((res) => {
+                setSearchResult(res.data);
+                setIsProcessingAPI(false);
+            })
+            .catch(() => {
+                setIsProcessingAPI(false);
+            });
+    }, [searchText]);
 
     const handleClear = () => {
         setSearchText('');
@@ -43,10 +57,9 @@ function Search() {
                 <div className={cx('search-results')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
+                        {searchResult.map((data) => (
+                            <AccountItem key={data.id} data={data} />
+                        ))}
                         <div className={cx('search-more')}>{`View all results for "${searchText}"`}</div>
                     </PopperWrapper>
                 </div>
@@ -65,12 +78,12 @@ function Search() {
                     }}
                     onFocus={() => setIsShowResult(true)}
                 />
-                {!!searchText && (
+                {!!searchText && !isProcessingAPI && (
                     <button className={cx('clear')} onClick={handleClear}>
                         <FontAwesomeIcon icon={faCircleXmark} />
                     </button>
                 )}
-                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                {isProcessingAPI && <Icon className={cx('loading')} icon="loading" />}
 
                 <button className={cx('search-btn')}>
                     <Icon icon="search" />
